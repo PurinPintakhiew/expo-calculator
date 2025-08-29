0c0e379f071afa9664e15fd6579f0a5928296d1a
Purin.P <dev.purin@gmail.com>
2025-08-29 23:46:14 +0700

    feat: main button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import './global.css';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='mt-5'>
-      <Text className='text-red-500'>Hello</Text>
-      <StatusBar style="auto" />
+    <View className="">
+      <View className='h-1/3'></View>
+      <View className='flex flex-row w-full'>
+        <View className='flex flex-row flex-wrap w-3/4'>
+          <TouchableOpacity className='w-1/3 flex aspect-square justify-center items-center border rounded-md'>
+            <Text>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='w-1/3 aspect-square justify-center items-center border rounded-md'>
+            <Text>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View className='flex flex-col w-1/4'>
+          <TouchableOpacity className='flex-1 justify-center items-center border rounded-md'>
+            <Text>Del</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-1 justify-center items-center border rounded-md'>
+            <Text>/</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-1 justify-center items-center border rounded-md'>
+            <Text>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-1 justify-center items-center border rounded-md'>
+            <Text>-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className='flex-1 justify-center items-center border rounded-md'>
+            <Text>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
