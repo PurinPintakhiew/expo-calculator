514f1559a48dd2cd36a4d6aa7adbcda6ae764b73
Purin.P <dev.purin@gmail.com>
2025-09-14 23:03:02 +0700

    feat: operator

--- App.tsx
+++ App.tsx
@@ -7,11 +7,26 @@ import { Button } from "components/Button";
 
 export default function App() {
   const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState('');
 
   const onHandelCalculator = (val: number) => {
     try {
-      const newResult = result.toString() + val.toString();
-      setResult(Number(newResult));
+      switch (operator) {
+        case 'plus':
+          setResult(Number(result + val));
+          break;
+        case 'minus':
+          setResult(Number(result - val));
+          break;
+        case 'multiply':
+          setResult(Number(result * val));
+          break;
+        case 'divide':
+          setResult(Number(result / val));
+          break;
+        default:
+          setResult(Number(result.toString() + val.toString()));
+      }
     } catch (error) {
       console.error(error);
     }
@@ -57,22 +72,22 @@ export default function App() {
           <Button label="AC" onPress={onHandelClear} type="action" />
           <Button label="( )" type="action" />
           <Button label="%" type="action" />
-          <Button label="÷" type="operator" />
+          <Button label="÷" type="operator" onPress={() => setOperator('divide')} />
 
           <Button label="7" onPress={() => onHandelCalculator(7)} />
           <Button label="8" onPress={() => onHandelCalculator(8)} />
           <Button label="9" onPress={() => onHandelCalculator(9)} />
-          <Button label="×" type="operator" />
+          <Button label="×" type="operator" onPress={() => setOperator('multiply')} />
 
           <Button label="4" onPress={() => onHandelCalculator(4)} />
           <Button label="5" onPress={() => onHandelCalculator(5)} />
           <Button label="6" onPress={() => onHandelCalculator(6)} />
-          <Button label="−" type="operator" />
+          <Button label="−" type="operator" onPress={() => setOperator('minus')} />
 
           <Button label="1" onPress={() => onHandelCalculator(1)} />
           <Button label="2" onPress={() => onHandelCalculator(2)} />
           <Button label="3" onPress={() => onHandelCalculator(3)} />
-          <Button label="+" type="operator" />
+          <Button label="+" type="operator" onPress={() => setOperator('plus')} />
 
           <Button label="." />
           <Button label="0" onPress={() => onHandelCalculator(0)} />
